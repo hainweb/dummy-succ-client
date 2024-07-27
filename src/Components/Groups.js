a1295a442cc42c6fe70e4a42fc23e4590f897e14
hainweb <hainofficialweb@gmail.com>
2024-07-27 13:28:15 +0530

    Update Groups.js

--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -36,7 +36,7 @@ function Groups() {
     };
 
     axios
-      .get("http://localhost:8080/chat/fetchGroups", config)
+      .get("https://dummy-succ-server.onrender.com/chat/fetchGroups", config)
       .then((response) => {
         console.log("Group Data from API ", response.data);
         SetGroups(response.data);
