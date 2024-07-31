6ab723b3809077db259f1af3c307865fa1254bae
hainweb <hainofficialweb@gmail.com>
2024-07-31 12:46:55 +0530

    Update MessageSelf.js

--- src/Components/MessageSelf.js
+++ src/Components/MessageSelf.js
@@ -1,14 +1,21 @@
 import React from "react";
 
+
 function MessageSelf({ props }) {
-  // console.log("Message self Prop : ", props);
+  const formattedTime = new Date(props.createdAt).toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  console.log("MessageSelf timestamp:", props.createdAt, "Formatted:", formattedTime);
+
   return (
     <div className="self-message-container">
       <div className="messageBox">
-        <p style={{ color: "black" }}>{props.content}</p>
-        {/* <p className="self-timeStamp" style={{ color: "black" }}>
-          12:00am
-        </p> */}
+        <p className="self-snd-msg" /*style={{ color: "black" }}*/>{props.content}</p>
+        <p className="self-timeStamp" style={{ color: "black" }}>
+          {formattedTime}
+        </p>
       </div>
     </div>
   );
