bdbf1a087b39164de07d099d088cca53d3948333
hainweb <hainofficialweb@gmail.com>
2024-07-31 12:46:25 +0530

    Update MessageOthers.js

--- src/Components/MessageOthers.js
+++ src/Components/MessageOthers.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./myStyles.css";
 import { useDispatch, useSelector } from "react-redux";
 
+
 function MessageOthers({ props }) {
   const dispatch = useDispatch();
   const lightTheme = useSelector((state) => state.themeKey);
-  // console.log("message others : ", props);
+
+  const formattedTime = new Date(props.createdAt).toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  console.log("MessageOthers timestamp:", props.createdAt, "Formatted:", formattedTime);
+
   return (
     <div className={"other-message-container" + (lightTheme ? "" : " dark")}>
       <div className={"conversation-container" + (lightTheme ? "" : " dark")}>
@@ -13,13 +21,13 @@ function MessageOthers({ props }) {
           {props.sender.name[0]}
         </p>
         <div className={"other-text-content" + (lightTheme ? "" : " dark")}>
-          <p className={"con-title" + (lightTheme ? "" : " dark")}>
+          <p className={"con-title-chatArea" + (lightTheme ? "" : " dark")}>
             {props.sender.name}
           </p>
           <p className={"con-lastMessage" + (lightTheme ? "" : " dark")}>
             {props.content}
           </p>
-          {/* <p className="self-timeStamp">12:00am</p> */}
+          <p className={"other-timeStamp" + (lightTheme ? "" : " dark")}>{formattedTime}</p>
         </div>
       </div>
     </div>
